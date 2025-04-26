1da3fa670f5b4815bf0b1d6255847280d3d3b02b
Guilherme Rodrigues <guilhermeaz.rodrigues@gmail.com>
2025-04-26 22:46:41 +0100

    ponto

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ import os
 # Adiciona o diretório pai ao sys.path para que os módulos possam ser importados corretamente
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from controller.produto_controller import ProdutoController # Importa a classe ProdutoController
 from view.opcoesView import OpcoesView # Importa a classe TerminalView
 
 def main():
